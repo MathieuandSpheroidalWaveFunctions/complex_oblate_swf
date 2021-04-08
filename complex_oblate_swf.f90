b90ef96d2d78fe9501e9498222504640114dbb96
Arnie Lee Van Buren <vanburenal@nc.rr.com>
2021-04-08 01:02:45 -0400

    initial commit, thanks to Brian Gladman

--- complex_oblate_swf.f90
+++ complex_oblate_swf.f90
@@ -29,14 +29,6 @@ module complex_oblate_swf
 !               angle coordinate eta for a given value of c and m and
 !               for a specified number of degrees l = m, m+1,..., m+lnum-1
 !
-!  Coblfcn provides function values for c complex = real(c) + i aimag(c)
-!  = cr + i ci, where the imaginary part ci often accounts for losses in
-!  wave propagation. Ci is assumed positive in coblfcn. If the user has
-!  a negative value for ci, just run coblfcn with ci positive instead
-!  and take the complex conjugate of the results, including the function
-!  values, eigenvalues, expansion coefficients, and normalization
-!  factors.
-!
 !  Coblfcn can be run in double precision, quadruple precision or a hybrid
 !  where the Bouwkamp procedure to refine the eigenvalues is run in quadruple
 !  precision while the remainder of the calculations are performed in double
